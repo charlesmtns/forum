bcf03f39959b6a16fc2dc997add2fce56cc8886c
Charles <charles.mtns@gmail.com>
2019-01-22 10:36:21 -0200

    filtros opcionais spring data

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,17 +1,14 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
-import br.com.alura.forum.model.Category;
-import br.com.alura.forum.model.Course;
-import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.TopicRepository;
 
@@ -22,9 +19,10 @@ public class TopicController {
 	private TopicRepository topicRepository;
 
 	@GetMapping(value = "/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<TopicBriefOutputDto> listTopics() {
+	public List<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch) {
 
-		List<Topic> topics = this.topicRepository.findAll();
+		Specification<Topic> topicSearchSpecification = topicSearch.build();
+		List<Topic> topics = this.topicRepository.findAll(topicSearchSpecification);
 
 		return TopicBriefOutputDto.listFromTopics(topics);
 	}
